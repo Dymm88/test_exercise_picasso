dbb81ce7f294ec7b3c3bbc10fa06b6089a21e3ce
Dmitriy <ulrht88@gmail.com>
2024-02-03 22:21:13 +0300

    task 2 - create serializers and endpoints

--- Exercise/app/views.py
+++ Exercise/app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .models import File
+from .serializers import FileSerializer
+from .tasks import process_file
+
+
+@api_view(['POST'])
+def upload_file(request):
+    file_serializer = FileSerializer(data=request.data)
+    if file_serializer.is_valid():
+        file_serializer.save()
+        process_file.delay(file_serializer.data['id'])
+        return Response(file_serializer.data,status=status.HTTP_201_CREATED)
+    return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def file_list(request):
+    files = File.objects.all()
+    serializer = FileSerializer(files, many=True)
+    return Response(serializer.data)
+
